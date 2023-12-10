2848b8eaf99843fce18f094e92a222041c485661
alexiiko <alexiiko.msl@gmail.com>
2023-12-10 12:43:46 +0100

    implemented card generating and terminal application

--- translator.py
+++ translator.py
@@ -1,28 +1,72 @@
 import json
+from language_codes import language_codes
 from googletrans import Translator
+from sys import exit
 
-FILE_NAME = "highlights.json"
-SOURCE_LANG = "en"
-DEST_LANG = "de"
+FILE_NAME = input("JSON file path with file name: ")
+
+SOURCE_LANG = ""
+DEST_LANG = ""
+
+source_lang_input = input("Source language: ")
+
+if source_lang_input.lower() in language_codes:
+    SOURCE_LANG = source_lang_input
+else:
+    print("ERROR: Source language not available")
+    exit()
+
+dest_lang_input = input("Destinated language: ")
+
+if dest_lang_input.lower() in language_codes:
+    DEST_LANG = dest_lang_input
+else:
+    print("ERROR: Destinated language not available")
+    exit()
+
 
 translator = Translator()
 
-vocabs = []
+marked_words = []
 
-translated_words_list = []
+marked_words_translated = []
 
-global object_
-with open(FILE_NAME, "r") as json_file:
-    object_ = json.load(json_file)
+global json_object
+try:
+    with open(FILE_NAME, "r") as json_file:
+        try:
+            json_object = json.load(json_file)
+        except:
+            print("ERROR: Provided file is false or file extension is not json.")
+            exit()
+except:
+    print("ERROR: Could not open file/ file path. Check if the provided path is right and if the file exists.")
+    exit()
 
-highlights = object_["highlights"]
+highlights = json_object["highlights"]
 
 for entry in highlights:
-    vocabs.append(entry["text"].strip("!.,:;"))
-
-for index in range(len(vocabs)):
     try:
-        translated_word = translator.translate(vocabs[index], dest=DEST_LANG, src=SOURCE_LANG)
-        translated_words_list.append(translated_word.text)
+        marked_words.append(entry["text"].strip("!.,:;"))
+    except:
+        print("ERROR: Json file does not correspond to the json schema.")
+        exit()
+
+print()
+
+print("Translating words...")
+
+vocabs = []
+vocabs_translated = []
+for index in range(len(marked_words)):
+    try:
+        translated_word = translator.translate(marked_words[index], dest=DEST_LANG, src=SOURCE_LANG)
+        marked_words_translated.append(translated_word.text)
+        vocabs.append(marked_words[index])
+        vocabs_translated.append(translated_word.text)
     except: 
-        print(f"Failed the translation for the word: {vocabs[index]}")
+        pass
+
+print("Translating done!")
+
+print()
